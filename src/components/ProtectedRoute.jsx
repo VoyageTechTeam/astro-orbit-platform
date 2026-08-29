f9b2f90ffdd999dd905c314b4643caade63e7630
Daniel Rion Majumder <danielmazumder@gmail.com>
2026-08-29 10:21:15 +0600

    Update ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,1 +1,36 @@
+import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
 
+export const RequireAuth = () => {
+  const token = localStorage.getItem('accessToken');[cite: 70]
+  if (!token) {
+    return <Navigate to="/login" replace />;
+  }
+  return <Outlet />;
+};
+
+export const HostOnlyRoute = () => {
+  const token = localStorage.getItem('accessToken');[cite: 70]
+  const userRole = localStorage.getItem('role');
+
+  if (!token) {
+    return <Navigate to="/login" replace />;
+  }
+  if (userRole !== 'host') {
+    return <Navigate to="/unauthorized" replace />;
+  }
+  return <Outlet />;
+};
+
+export const TravelerOnlyRoute = () => {
+  const token = localStorage.getItem('accessToken');[cite: 70]
+  const userRole = localStorage.getItem('role');
+
+  if (!token) {
+    return <Navigate to="/login" replace />;
+  }
+  if (userRole !== 'traveler') {
+    return <Navigate to="/unauthorized" replace />;
+  }
+  return <Outlet />;
+};
