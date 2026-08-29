b8ff931d08e29c85ec6a90284a527e0472096311
Daniel Rion Majumder <danielmazumder@gmail.com>
2026-08-30 01:28:04 +0600

    Update redis.js

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,7 +1,7 @@
 const Redis = require('ioredis');
 
 const redisClient = new Redis({
-  host: process.env.REDIS_HOST || '127.0.0.1',[cite: 61]
+  host: process.env.REDIS_HOST || '127.0.0.1',
   port: parseInt(process.env.REDIS_PORT || '6379', 10),[cite: 61]
   maxRetriesPerRequest: null, // Required by BullMQ[cite: 66]
 });
