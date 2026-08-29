29fa94653b93df7ab8efa2bda15d5f68607a6d50
Daniel Rion Majumder <danielmazumder@gmail.com>
2026-08-30 01:35:45 +0600

    Update redis.js

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,13 +1,9 @@
 const Redis = require('ioredis');
 
-const redisClient = new Redis({
+const redis = new Redis({
   host: process.env.REDIS_HOST || '127.0.0.1',
-  port: parseInt(process.env.REDIS_PORT || '6379', 10),[cite: 61]
-  maxRetriesPerRequest: null, // Required by BullMQ[cite: 66]
+  port: parseInt(process.env.REDIS_PORT || '6379', 10),
+  password: process.env.REDIS_PASSWORD || undefined,
 });
 
-redisClient.on('error', (err) => {
-  console.error('[Redis Error]', err);
-});
-
-module.exports = redisClient;
+module.exports = redis;
